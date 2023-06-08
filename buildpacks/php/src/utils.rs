b29be992431f8cc29a14b22f484e65818f407b90
David Zuelke <dzuelke@salesforce.com>
2023-06-08 19:46:34 +0200

    macro for static regexes using Rust 1.70's stable std::sync::OnceLock

--- buildpacks/php/src/utils.rs
+++ buildpacks/php/src/utils.rs
@@ -4,6 +4,14 @@ use std::path::Path;
 use std::process::{Command, ExitStatus};
 use tar::Archive;
 
+macro_rules! regex {
+    ($re:literal $(,)?) => {{
+        static RE: std::sync::OnceLock<regex::Regex> = std::sync::OnceLock::new();
+        RE.get_or_init(|| regex::Regex::new($re).unwrap())
+    }};
+}
+pub(crate) use regex;
+
 pub(crate) fn download_and_unpack_gzip(
     uri: &str,
     destination: &Path,
