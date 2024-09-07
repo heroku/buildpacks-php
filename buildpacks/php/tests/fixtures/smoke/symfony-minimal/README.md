9a39bd8d3cd0afc8750a687cd7d6b5ae697058da
Cedric Ziel <cedric@cedric-ziel.com>
2024-09-07 13:11:39 +0200

    Support composer scripts as objects to support symfony apps
    Closes: #81

---
+++ buildpacks/php/tests/fixtures/smoke/symfony-minimal/README.md
@@ -0,0 +1,7 @@
+# Minimal symfony app
+
+Serve:
+
+```bash
+php -S 127.0.0.1:8000 -t public/
+```
---
+++ buildpacks/php/tests/fixtures/smoke/symfony-minimal/src/Controller/HomepageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomepageController extends AbstractController
+{
+    #[Route('/', name: 'homepage')]
+    public function __invoke(): Response
+    {
+        return new Response('Symfony Minimal');
+    }
+}
