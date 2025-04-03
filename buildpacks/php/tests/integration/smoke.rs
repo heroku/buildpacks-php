199baa932d46994f9b19e440648b808dcdb63620
Schneems <richard.schneeman+foo@gmail.com>
2025-04-03 14:14:35 -0500

    Add a smoke test that passes
    This test passes but does not exercise the behavior we want to test against.

--- buildpacks/php/tests/integration/smoke.rs
+++ buildpacks/php/tests/integration/smoke.rs
@@ -5,8 +5,9 @@
 //!
 //! These tests are strictly happy-path tests and do not assert any output of the buildpack.
 
-use crate::utils::{builder, default_buildpacks, smoke_test};
-use libcnb_test::BuildpackReference;
+use crate::utils::{builder, default_buildpacks, smoke_test, target_triple};
+use indoc::formatdoc;
+use libcnb_test::{BuildConfig, BuildpackReference, TestRunner};
 
 #[test]
 #[ignore = "integration test"]
@@ -17,6 +18,30 @@ fn smoke_test_bundled_hello_world_app() {
         vec![BuildpackReference::CurrentCrate],
         "Hello World",
     );
+}
+
+#[test]
+#[ignore = "integration test"]
+fn smoke_test_composer_json_scripts_as_objects() {
+    // TODO modify the composer.json to assert https://github.com/heroku/buildpacks-php/issues/81
+    let app_dir = "tests/fixtures/smoke/hello-world";
+
+    let build_config = BuildConfig::new(builder(), app_dir)
+        .buildpacks(vec![BuildpackReference::CurrentCrate])
+        .target_triple(target_triple(builder()))
+        .to_owned();
+
+    TestRunner::default().build(&build_config, |context| {
+        assert_eq!(
+            formatdoc! {"
+                /layers/heroku_php/platform/bin/php
+                /layers/heroku_php/platform/bin/php
+                /layers/heroku_php/platform/bin/php
+            "}
+            .trim(),
+            context.run_shell_command("which -a php").stdout.trim()
+        );
+    });
 }
 
 #[test]
