dcf7b82258197e031f974711f6f2153970f44222
Schneems <richard.schneeman+foo@gmail.com>
2025-04-08 15:20:27 -0500

    Simplify smoke test
    Uses the built in libcnb `app_dir_preprocessor` to achieve a similar directory modification https://docs.rs/libcnb-test/0.28.1/libcnb_test/struct.BuildConfig.html#method.app_dir_preprocessor.
    
    Removes the `run_shell_command` call as it's not needed, we just need to assert that the app built correctly.

--- buildpacks/php/tests/integration/smoke.rs
+++ buildpacks/php/tests/integration/smoke.rs
@@ -5,12 +5,10 @@
 //!
 //! These tests are strictly happy-path tests and do not assert any output of the buildpack.
 
-use crate::utils::{builder, copy_dir_all, default_buildpacks, smoke_test, target_triple};
-use indoc::formatdoc;
+use crate::utils::{builder, default_buildpacks, smoke_test, target_triple};
 use libcnb_test::{BuildConfig, BuildpackReference, TestRunner};
 use serde_json::json;
 use std::fs;
-use std::path::PathBuf;
 
 #[test]
 #[ignore = "integration test"]
@@ -26,57 +24,40 @@ fn smoke_test_bundled_hello_world_app() {
 #[test]
 #[ignore = "integration test"]
 fn smoke_test_composer_json_scripts_as_objects() {
-    let temp = tempfile::tempdir().unwrap();
-    let source = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-        .join("tests/fixtures/smoke/hello-world")
-        .canonicalize()
-        .unwrap();
-    let app_dir = temp.path();
-    copy_dir_all(source, app_dir).unwrap();
-
-    let mut composer_json = serde_json::from_str::<serde_json::Map<_, _>>(
-        &fs::read_to_string(app_dir.join("composer.json")).unwrap(),
-    )
-    .unwrap();
-
-    composer_json.insert(
-        "scripts".to_string(),
-        json!({
-            "auto-scripts": {
-                "cache:clear": "echo 'cache:clear'",
-                "assets:install %PUBLIC_DIR%": "echo 'assets:install'",
-                "importmap:install": "echo 'importmap:install'"
-            },
-            "post-install-cmd": [
-                "@auto-scripts"
-            ],
-            "post-update-cmd": [
-                "@auto-scripts"
-            ]
-        }),
-    );
-    fs::write(
-        app_dir.join("composer.json"),
-        serde_json::to_string(&composer_json).unwrap(),
-    )
-    .unwrap();
-
-    let build_config = BuildConfig::new(builder(), app_dir)
+    let build_config = BuildConfig::new(builder(), "tests/fixtures/smoke/hello-world")
         .buildpacks(vec![BuildpackReference::CurrentCrate])
         .target_triple(target_triple(builder()))
+        .app_dir_preprocessor(|app_dir| {
+            let mut composer_json = serde_json::from_str::<serde_json::Map<_, _>>(
+                &fs::read_to_string(app_dir.join("composer.json")).unwrap(),
+            )
+            .unwrap();
+
+            composer_json.insert(
+                "scripts".to_string(),
+                json!({
+                    "auto-scripts": {
+                        "cache:clear": "echo 'cache:clear'",
+                        "assets:install %PUBLIC_DIR%": "echo 'assets:install'",
+                        "importmap:install": "echo 'importmap:install'"
+                    },
+                    "post-install-cmd": [
+                        "@auto-scripts"
+                    ],
+                    "post-update-cmd": [
+                        "@auto-scripts"
+                    ]
+                }),
+            );
+            fs::write(
+                app_dir.join("composer.json"),
+                serde_json::to_string(&composer_json).unwrap(),
+            )
+            .unwrap();
+        })
         .to_owned();
 
-    TestRunner::default().build(&build_config, |context| {
-        assert_eq!(
-            formatdoc! {"
-                /layers/heroku_php/platform/bin/php
-                /layers/heroku_php/platform/bin/php
-                /layers/heroku_php/platform/bin/php
-            "}
-            .trim(),
-            context.run_shell_command("which -a php").stdout.trim()
-        );
-    });
+    TestRunner::default().build(&build_config, |_context| {});
 }
 
 #[test]
