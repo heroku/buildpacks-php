cdee35a03ee8878f6065e5f6bde06dfff7907899
David Zuelke <dzuelke@salesforce.com>
2025-06-12 16:45:07 +0200

    explain why we're using rstest master, and what 'cargo::rerun-if-changed' in build.rs does

--- buildpacks/php/build.rs
+++ buildpacks/php/build.rs
@@ -1,3 +1,5 @@
 pub fn main() {
+    // we want to re-compile anytime a test fixture changes
+    // reason: an rstest macro generates test cases for each fixture in that directory
     println!("cargo::rerun-if-changed=tests/fixtures");
 }
