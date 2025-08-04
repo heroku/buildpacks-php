ffecfec0db087c1936ef10eff85a21876c9bc827
David Zülke <dzuelke@salesforce.com>
2025-08-05 00:28:34 +0200

    Retry downloads during bootstrapping (#226)
    * say yes to the Box
    
    * timeout TODO comment update (for ureq v3)
    
    * Retry downloads during bootstrapping
    
    GUS-W-19239585

--- buildpacks/php/CHANGELOG.md
+++ buildpacks/php/CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Changed
+
+- Retry downloads during bootstrapping ([#226](https://github.com/heroku/buildpacks-php/pull/226))
+
 ## [1.0.6] - 2025-07-31
 
 ### Added
--- buildpacks/php/src/utils.rs
+++ buildpacks/php/src/utils.rs
@@ -1,6 +1,7 @@
 use flate2::read::GzDecoder;
 use std::io;
 use std::path::{Component, Path, PathBuf};
+use std::time::Duration;
 use tar::Archive;
 
 macro_rules! regex {
@@ -17,7 +18,7 @@ pub(crate) use regex;
 pub(crate) enum DownloadUnpackError {
     Io(io::Error),
     // Boxed to prevent `large_enum_variant` Clippy errors since `ureq::Error` is massive.
-    Request(Box<ureq::Error>), // TODO: does this still need boxing?
+    Request(Box<ureq::Error>),
 }
 
 #[allow(unused)]
@@ -48,7 +49,7 @@ pub(crate) fn download_and_unpack_tgz_with_components_stripped_and_only_entries_
     strip_components: usize,
     extract_only_prefix: impl AsRef<Path>,
 ) -> Result<(), DownloadUnpackError> {
-    Archive::new(GzDecoder::new(download(uri)?))
+    Archive::new(GzDecoder::new(download_with_retry(uri)?))
         .entries()
         .map_err(DownloadUnpackError::Io)? // discard invalid entries
         .filter_map(Result::ok)
@@ -75,9 +76,29 @@ pub(crate) fn download_and_unpack_tgz_with_components_stripped_and_only_entries_
 }
 
 fn download(uri: &str) -> Result<Box<dyn io::Read + Send + Sync + 'static>, DownloadUnpackError> {
-    // TODO: Timeouts: https://docs.rs/ureq/latest/ureq/struct.AgentBuilder.html?search=timeout
+    // TODO: Timeouts once we move to ureq v3 (as it has ConfigBuilder::timeout_resolve())
     Ok(ureq::get(uri)
         .call()
         .map_err(|err| DownloadUnpackError::Request(Box::new(err)))?
         .into_reader())
+}
+
+fn download_with_retry(
+    uri: &str,
+) -> Result<Box<dyn io::Read + Send + Sync + 'static>, DownloadUnpackError> {
+    let backoff =
+        exponential_backoff::Backoff::new(3, Duration::from_secs(1), Duration::from_secs(10));
+
+    let mut backoff_durations = backoff.into_iter();
+    loop {
+        match download(uri) {
+            result @ Ok(_) => return result,
+            result @ Err(_) => match backoff_durations.next() {
+                None | Some(None) => return result,
+                Some(Some(backoff_duration)) => {
+                    std::thread::sleep(backoff_duration);
+                }
+            },
+        }
+    }
 }
