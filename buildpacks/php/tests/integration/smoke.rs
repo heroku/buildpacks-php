eb299e79bbcc07cd3c9d460cfaf9c91428207498
David Zuelke <dzuelke@salesforce.com>
2025-10-07 18:00:14 +0200

    Update polyfill smoke test to check against exact output

--- buildpacks/php/tests/integration/smoke.rs
+++ buildpacks/php/tests/integration/smoke.rs
@@ -10,7 +10,8 @@ use crate::utils::{
     target_triple,
 };
 use fs_err as fs;
-use libcnb_test::{BuildConfig, BuildpackReference, TestRunner};
+use indoc::formatdoc;
+use libcnb_test::{BuildConfig, BuildpackReference, TestRunner, assert_contains_match};
 use serde_json::json;
 
 #[test]
@@ -98,5 +99,38 @@ fn smoke_test_php_polyfills() {
         .target_triple(target_triple(builder()))
         .to_owned();
 
-    TestRunner::default().build(&build_config, |_context| {});
+    TestRunner::default().build(&build_config, |context| {
+        assert_contains_match!(
+            context.pack_stdout,
+            formatdoc! {r"
+                - Installing platform packages
+                  - php {version_triple}
+                  - composer {version_triple}
+                  - ext-bcmath {bundled}
+                  - ext-gd {bundled}
+                  - ext-imagick {version_triple}
+                  - ext-intl {bundled}
+                  - ext-oauth {version_triple}
+                  - ext-redis {version_triple}
+                  - ext-soap {bundled}
+                  - Attempting native package installs for dzuelke/ext-pq-polyfill
+                    - ext-raphf {version_triple}
+                    - ext-pq {version_triple}
+                  - Attempting native package installs for phpseclib/mcrypt_compat
+                    - No suitable native version of heroku-sys/ext-mcrypt available
+                  - Attempting native package installs for symfony/polyfill-ctype
+                    - ext-ctype {enabled}
+                  - Attempting native package installs for symfony/polyfill-mbstring
+                    - ext-mbstring {bundled}
+                - Installing web servers
+                  - nginx {version_triple}
+                  - apache {version_triple}
+                  - boot-scripts {version_triple}
+                ",
+                version_triple = r"\(\d+\.\d+\.\d+\)",
+                bundled = r"\(bundled with php\)",
+                enabled = r"\(already enabled\)"
+            }
+        );
+    });
 }
