b0c7b78577645d46e6ee4862eac53bef281bb352
David Zuelke <dzuelke@salesforce.com>
2025-10-21 16:19:44 -0400

    Integration test for failed platform package install
    GUS-W-19848835

--- buildpacks/php/tests/integration/platform.rs
+++ buildpacks/php/tests/integration/platform.rs
@@ -46,3 +46,45 @@ fn platform_test_polyfills() {
         );
     });
 }
+
+#[test]
+#[ignore = "integration test"]
+fn platform_test_failure() {
+    let build_config = BuildConfig::new(builder(), "tests/fixtures/platform/installation/failure")
+        .buildpacks(vec![BuildpackReference::CurrentCrate])
+        .target_triple(target_triple(builder()))
+        .expected_pack_result(libcnb_test::PackResult::Failure)
+        .to_owned();
+
+    TestRunner::default().build(&build_config, |context| {
+        assert_contains_match!(
+            context.pack_stdout,
+            formatdoc! {r"
+                ! Failed to install platform dependencies
+                !
+                ! Your platform requirements \(for runtimes and extensions\) could
+                ! not be resolved to an installable set of dependencies\.
+                !
+                ! This usually means that you \(or packages you are using\) depend
+                ! on a combination of PHP versions and/or extensions that are
+                ! currently not available on Heroku\.
+                !
+                ! The following is the full output from the installation attempt:
+                !
+                ((?s)^!.*$)+
+                !
+                ! Please verify that all requirements for runtime versions in
+                ! 'composer\.lock' are compatible with the list below, and ensure
+                ! all required extensions are available for the desired runtimes\.
+                !
+                ! When choosing a PHP runtimes and extensions, please also ensure
+                ! they are available on your app's stack, and, if necessary, choose
+                ! a different stack after consulting the article below\.
+                !
+                ! For a list of supported runtimes & extensions on Heroku, please
+                ! refer to: https://devcenter.heroku.com/articles/php-support
+                "
+            }
+        );
+    });
+}
